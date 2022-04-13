0d22ef1ffbf34f78ac013673e3022fdd8d7d6eaa
ly <liyangfamily@qq.com>
2022-04-13 13:48:37 +0800

    [config][ChromX][mainwindow] update ChromX project
    [what] 1、更新主项目
    
    [why]
    
    [how]

--- src/ChromXApp/mainwindow.cpp
+++ src/ChromXApp/mainwindow.cpp
@@ -3,12 +3,15 @@
 
 #include <QDebug>
 #include <CCE_Core/CCEUIHelper>
+#include <CCE_CommunicatEngine/CCEMainCtrlPackage>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     qDebug()<<CCEUIHelper::appName();
+    CCEMainCtrlPackage_WriteHardwareVersion test(0x20);
+    qDebug()<<CCEUIHelper::byteArrayToHexStr(test.build().getDataToSend());
 }
 
 MainWindow::~MainWindow()
